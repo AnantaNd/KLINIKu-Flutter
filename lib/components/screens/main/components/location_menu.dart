ffe8f298dbfb7c3c90f50247cc387a977a179f07
AnantaNd <ananta.nov2@gmail.com>
2022-06-18 16:42:22 +0700

    add map with mapbox

--- lib/components/screens/main/components/location_menu.dart
+++ lib/components/screens/main/components/location_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kliniku/const.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong2/latlong.dart";
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key}) : super(key: key);
@@ -8,14 +9,44 @@ class LocationPage extends StatefulWidget {
   State<LocationPage> createState() => _LocationPageState();
 }
 
+const latitude = -6.9775495;
+const longitude = 107.6299839;
+
 class _LocationPageState extends State<LocationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: secondaryColor,
-      body: Center(
-        child: Text("Location"),
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(latitude, longitude),
+        zoom: 15.0,
       ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate:
+              "https://api.mapbox.com/styles/v1/anantand/cl4dkq6jz000315s2r98vukpl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5hbnRhbmQiLCJhIjoiY2w0N3c5cXVyMGJ5dTNkcDhqOWJoeHR5ciJ9.WgGznLo-f-ISwe9lFVsoWg",
+          additionalOptions: {
+            'accessToken':
+                'pk.eyJ1IjoiYW5hbnRhbmQiLCJhIjoiY2w0N3c5cXVyMGJ5dTNkcDhqOWJoeHR5ciJ9.WgGznLo-f-ISwe9lFVsoWg',
+            'id': 'mapbox.country-boundaries-v1'
+          },
+        ),
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(latitude, longitude), //mark klinik telkom
+              builder: (ctx) => Container(
+                child: Icon(
+                  Icons.location_on,
+                  size: 40.0,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
